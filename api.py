aff4a622ff40c8c11e8698204e8d5df2d9723826
Brian Abelson <brianabelson@gmail.com>
2013-10-14 01:25:10 -0400

    added make.sh

--- api.py
+++ api.py
@@ -55,4 +55,4 @@ def query():
 
 if __name__ == "__main__":
   app.debug = True
-  app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0', port=8000)
---
+++ make.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+sudo sh ec2-utils/install-redis.sh
+sudo yum install python27
+sudo easy_intall pip
+sudo pip install -r requirements.pip
